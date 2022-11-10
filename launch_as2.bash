002ed1ab120f81c059583517fc2ff74e4b5412dd
pariaspe <pedro.ariasp@upm.es>
2022-11-10 15:03:15 +0100

    initial commit

---
+++ launch_as2.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Arguments
+drone_namespace='cf'
+
+# cf_uri="radio://0/40/2M/E7E7E7E7E7"
+cf_uri="radio://0/66/2M/E7E7E7E744"
+aideck_ip="192.168.0.140"
+aideck_port="5000"
+
+source ./launch_tools.bash
+
+new_session $drone_namespace
+new_window 'crazyflie_interface' "ros2 launch crazyflie_platform crazyflie_platform_launch.py \
+    drone_id:=$drone_namespace \
+    drone_URI:=$cf_uri \
+    estimator_type:=2 \
+    controller_type:=1"
+
+new_window 'state_estimator' "ros2 launch basic_state_estimator basic_state_estimator_launch.py \
+    namespace:=$drone_namespace \
+    odom_only:=True"
+
+new_window 'controller_manager' "ros2 launch controller_manager controller_manager_launch.py \
+    namespace:=$drone_namespace  \
+    use_bypass:=False \
+    config:=drone_config/controller.yaml"
+
+# new_window 'traj_generator' "ros2 launch trajectory_generator trajectory_generator_launch.py  \
+#     drone_id:=$drone_namespace "
+
+new_window 'basic_behaviours' "ros2 launch as2_basic_behaviours all_basic_behaviours_launch.py \
+    drone_id:=$drone_namespace \
+    config_goto:=drone_config/goto.yaml \
+    config_takeoff:=drone_config/takeoff.yaml"
+
+# echo -e "Launched drone $drone_namespace. For attaching to the session, run: \n  \t $ tmux a -t $drone_namespace"
+tmux a -t :0
---
+++ launch_tools.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+WINDOW_ID=0
+SESSION=""
+
+function new_session() {
+    SESSION=$1
+    # # Create new session  (-2 allows 256 colors in the terminal, -s -> session name, -d -> not attach to the new session)
+    # tmux -2 new-session -d -s $SESSION
+}
+
+function new_window() {
+  if [ $WINDOW_ID -eq 0 ]; then
+    # Kill any previous session (-t -> target session, -a -> all other sessions )
+    tmux kill-session -t $SESSION
+    # Create new session  (-2 allows 256 colors in the terminal, -s -> session name, -d -> not attach to the new session)
+    tmux -2 new-session -d -s $SESSION
+
+    # send-keys writes the string into the sesssion (-t -> target session , C-m -> press Enter Button)
+    tmux rename-window -t $SESSION:0 "$1"
+    tmux send-keys -t $SESSION:0 "$2" C-m
+
+  else
+    tmux new-window -t $SESSION:$WINDOW_ID -n "$1"
+    tmux send-keys -t $SESSION:$WINDOW_ID "$2" C-m
+  fi
+  WINDOW_ID=$((WINDOW_ID+1))
+}
+
+function send_ctrl_c_tmux_session() {
+    session_name=$1
+
+    if [ -z "$session_name" ]; then
+        echo "Usage: $0 <session_name>"
+        exit 1
+    fi
+
+    windows=$(tmux list-windows -t "$session_name" | awk '{print $1}'| tr -d ':')
+    for window in $windows; do
+        # tmux kill-window -t "$session_name:$window"
+        echo "Killed window $window"
+        tmux send-keys -t "$session_name:$window" C-c
+        tmux send-keys -t "$session_name:$window" "echo 'Killed window $window'" C-m
+        # tmux send-keys -t "$session_name:$window" C-c
+    done
+    n_windows=$(tmux list-windows -t "$session_name" | wc -l)
+    sleep "$n_windows"
+    tmux kill-session -t "$session_name"
+}
