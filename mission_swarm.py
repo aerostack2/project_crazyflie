ab40bf4cb2aaade207f98a8de4a2435e1a3f1ce9
Javilinos <javier.mdeza@alumnos.upm.es>
2023-05-18 11:54:20 +0200

    swarm and solo mission

--- mission_swarm.py
+++ mission_swarm.py
@@ -79,35 +79,43 @@ def print_status(drone_interface: DroneInterface):
         drone_interface.get_logger().info(str(drone_interface.go_to.status))
 
 
-def drone_run(drone_interface: DroneInterface):
+def drone_run(drone_interface0: DroneInterface, drone_interface1: DroneInterface):
     """ Run the mission """
 
     sleep_time = 2.0
-    drone_interface.follow_reference = FollowReferenceModule(drone_interface)
+    drone_interface0.follow_reference = FollowReferenceModule(drone_interface0)
+    drone_interface1.follow_reference = FollowReferenceModule(drone_interface1)
     print("Start mission")
 
-    if confirm([drone_interface], "Takeoff"):
+    if confirm([drone_interface0], "Takeoff"):
     ##### ARM OFFBOARD #####
-        drone_interface.offboard()
-        drone_interface.arm()
+        drone_interface0.offboard()
+        drone_interface0.arm()
+        drone_interface1.offboard()
+        drone_interface1.arm()
 
         ##### TAKE OFF #####
         print("Take Off")
-        drone_interface.takeoff(1.0, 0.5)
+        drone_interface0.takeoff(1.0, 0.5)
+        drone_interface1.takeoff(1.0, 0.5)
         print("Take Off done")
 
-    if confirm([drone_interface], "go to initial position"):
-        drone_interface.go_to.go_to_point(point=[0.0, -1.0, 3.0], speed=0.5)
+    if confirm([drone_interface0], "go to initial position"):
+        drone_interface0.go_to.go_to_point(point=[0.0, -1.0, 3.0], speed=0.5)
+        drone_interface1.go_to.go_to_point(point=[0.0, 1.0, 3.0], speed=0.5)
 
-    if confirm([drone_interface], "reference"):
+    if confirm([drone_interface0], "reference"):
         print('going to reference')
-        drone_interface.follow_reference.follow_reference_with_yaw(1.0, 0, 1.0, 'point_0', 0.5, 0.5, 0.5, 3.14)
+        drone_interface0.follow_reference.follow_reference_with_yaw(1.0, 0, 1.0, 'point_0', 0.5, 0.5, 0.5, 3.14)
+        drone_interface1.follow_reference.follow_reference_with_yaw(-1.0, 0, 1.0, 'point_0', 0.5, 0.5, 0.5, 0.0)
     
-    if confirm([drone_interface], "stop reference"):
-        drone_interface.follow_reference.stop()
+    if confirm([drone_interface0], "stop reference"):
+        drone_interface0.follow_reference.stop()
+        drone_interface1.follow_reference.stop()
 
-    if confirm([drone_interface], "land"):
-        drone_interface.land(speed=0.5)
+    if confirm([drone_interface0], "land"):
+        drone_interface0.land(speed=0.5)
+        drone_interface1.land(speed=0.5)
 
 
 if __name__ == '__main__':
@@ -128,11 +136,13 @@ if __name__ == '__main__':
 
     rclpy.init()
 
-    uav_name = ["cf0"]
+    uav_name = ["cf0", "cf1"]
     uav_0 = DroneInterface(uav_name[0], verbose=False, use_sim_time=False)
-    drone_run(uav_0)
+    uav_1 = DroneInterface(uav_name[1], verbose=False, use_sim_time=False)
+    drone_run(uav_0, uav_1)
 
     uav_0.shutdown()
+    uav_1.shutdown()
     rclpy.shutdown()
 
     print("Clean exit")
